12c2aaf1018e8e1e77a343cf58d42b05988d755f
Adrian Azeredo Dos Anjos <137955973+AFootGolf9@users.noreply.github.com>
2023-07-24 22:33:28 -0300

    Update AreTheyThe"same".py

--- pythonCode/AreTheyThe"same".py
+++ pythonCode/AreTheyThe"same".py
@@ -1,6 +1,4 @@
 def comp(array1, array2):
-    print(array1)
-    print(array2)
     if(array1 is None or array2 is None):
         return False
     for elem in array1:
@@ -9,6 +7,5 @@ def comp(array1, array2):
     for elem in array2:
         num = pow(elem, 0.5)
         if array1.count(num) != array2.count(elem) and array1.count(-num) != array2.count(elem):
-            print(elem)
             return False
     return True
