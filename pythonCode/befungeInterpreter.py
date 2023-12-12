f918b8e30b5c159c50ec984eda6aa73fad51e1a5
Adrian dos Anjos <adriandosanjos000@gmail.com>
2023-12-12 10:57:23 -0300

    update

--- pythonCode/befungeInterpreter.py
+++ pythonCode/befungeInterpreter.py
@@ -11,22 +11,20 @@ def interpret(code):
     difX = 1
     difY = 0
 
+    code = code.split('\n')
+
     while(code[x][y] != '@'):
         match(code[x][y]):
             case ' ':
-                break
+                nothing = 0
             case '+':
                 stack.append(stack.pop() + stack.pop())
-                break
             case '-':
                 stack.append(stack.pop() - stack.pop())
-                break
             case '*':
                 stack.append(stack.pop() * stack.pop())
-                break
             case '/':
                 stack.append(stack.pop() / stack.pop())
-                break
             case '%':
                 a = stack.pop()
                 b = stack.pop()
@@ -34,54 +32,54 @@ def interpret(code):
                     stack.append(0)
                 else:
                     stack.append(b%a)
-                break
+                
             case '!':
                 if stack.pop == 0:
                     stack.append(1)
                 else:
                     stack.append(0)
-                break
+                
             case '`':
                 if stack.pop() < stack.pop():
                     stack.append(1)
                 else:
                     stack.append(0)
-                break
+                
             case '>':
                 difX = 1
                 dify = 0
-                break
+                
             case '<':
                 difX = -1
                 dify = 0
-                break
+                
             case '^':
                 difX = 0
                 dify = -1
-                break
+                
             case 'v':
                 difX = 0
                 dify = 1
-                break
+                
             case '?':
                 match(random.randrange(0,4)):
                     case 0:
                         difX = 1
                         dify = 0
-                        break
+                        
                     case 1:
                         difX = -1
                         dify = 0
-                        break
+                        
                     case 2:
                         difX = 0
                         dify = -1
-                        break
+                        
                     case 3:
                         difX = 0
                         dify = 1
-                        break
-                break
+                        
+                
             case '_':
                 if stack.pop() == 0:
                     difx = 1
@@ -89,7 +87,7 @@ def interpret(code):
                 else:
                     difx = -1
                     dify = 0
-                break
+                
             case '|':
                 if stack.pop() == 0:
                     dify = 1
@@ -97,53 +95,53 @@ def interpret(code):
                 else:
                     dify = -1
                     difx = 0
-                break
+                
             case '\"':
                 stringMode = True
-                break
+                
             case ':':
                 a = stack.pop()
                 stack.append(a)
                 stack.append(a)
-                break
+                
             case '\\':
                 match(stack.length()):
                     case 1:
                         stack.append(0)
-                        break
+                        
                     case 0:
                         stack.append(0)
                         stack.append(0)
-                        break
+                        
                     case _:
                         a = stack.pop()
                         b = stack.pop()
                         stack.append(a)
                         stack.append(b)
-                break
+                
             case '$':
                 stack.pop()
-                break
+                
             case '.':
                 output += stack.pop()
-                break
+                
             case ',':
                 output += ord(stack.pop())
-                break
+                
             case '#':
                 x += difX
                 y += dify
-                break
+                
             case 'p':
                 a = stack.pop()
                 b = stack.pop()
                 code[b][a] = stack.pop()
-                break
+                
             case 'g':
                 a = stack.pop()
                 b = stack.pop()
                 stack.append(code[b][a])
-                break
+                
             case _:
                 stack.append(code[x][y])
         
@@ -152,3 +150,5 @@ def interpret(code):
 
     # TODO: Interpret the code!
     return output
+
+print(interpret(">987v>.v\nv456<  :\n>321 ^ _@"))
