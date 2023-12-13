1a02fc47d0990ff361a06719dd1a2cd566e072d5
Adrian dos Anjos <adriandosanjos000@gmail.com>
2023-12-13 20:02:33 -0300

    update

--- pythonCode/befungeInterpreter.py
+++ pythonCode/befungeInterpreter.py
@@ -10,123 +10,128 @@ def interpret(code):
     y=0
     difY = 1
     difX = 0
+    stringMode = False
+
 
     code = code.split('\n')
 
     while(code[x][y] != '@'):
-        match code[x][y]:
-            case ' ':
-                nothing = 0
-            case '+':
-                stack.append(stack.pop() + stack.pop())
-            case '-':
-                stack.append(stack.pop() - stack.pop())
-            case '*':
-                stack.append(stack.pop() * stack.pop())
-            case '/':
-                stack.append(stack.pop() / stack.pop())
-            case '%':
-                a = stack.pop()
-                b = stack.pop()
-                if x == 0:
-                    stack.append(0)
-                else:
-                    stack.append(b%a)
-            case '!':
-                if stack.pop == 0:
-                    stack.append(1)
-                else:
-                    stack.append(0)
-            case '`':
-                if stack.pop() < stack.pop():
-                    stack.append(1)
-                else:
-                    stack.append(0)
-            case '>':
-                difY = 1
-                difX = 0
-            case '<':
-                difY = -1
-                difX = 0
-            case '^':
-                difY = 0
-                difX = -1
-            case 'v':
-                difY = 0
-                difX = 1
-            case '?':
-                match random.randrange(0,4):
-                    case 0:
-                        difY = 1
-                        difX = 0
-                    case 1:
-                        difY = -1
-                        difX = 0
-                    case 2:
-                        difY = 0
-                        difX = -1
-                    case 3:
-                        difY = 0
-                        difX = 1
-            case '_':
-                if stack.pop() == 0:
+        if stringMode:
+            if code[x][y] == '\"':
+                stringMode == False
+            stack.append(int(code[x][y]))
+        else:
+            match code[x][y]:
+                case ' ':
+                    nothing = 0
+                case '+':
+                    stack.append(stack.pop() + stack.pop())
+                case '-':
+                    stack.append(stack.pop() - stack.pop())
+                case '*':
+                    stack.append(stack.pop() * stack.pop())
+                case '/':
+                    stack.append(stack.pop() / stack.pop())
+                case '%':
+                    a = stack.pop()
+                    b = stack.pop()
+                    if x == 0:
+                        stack.append(0)
+                    else:
+                        stack.append(b%a)
+                case '!':
+                    if stack.pop == 0:
+                        stack.append(1)
+                    else:
+                        stack.append(0)
+                case '`':
+                    if stack.pop() < stack.pop():
+                        stack.append(1)
+                    else:
+                        stack.append(0)
+                case '>':
                     difY = 1
                     difX = 0
-                else:
+                case '<':
                     difY = -1
                     difX = 0
-            case '|':
-                if stack.pop() == 0:
-                    difX = 1
+                case '^':
                     difY = 0
-                else:
                     difX = -1
+                case 'v':
                     difY = 0
-            case '\"':
-                stringMode = True
-            case ':':
-                if len(stack )== 0:
-                    stack.append(0)
-                else:
-                    a = stack.pop()
-                    stack.append(a)
-                    stack.append(a)
-            case '\\':
-                match len(stack):
-                    case 1:
+                    difX = 1
+                case '?':
+                    match random.randrange(0,4):
+                        case 0:
+                            difY = 1
+                            difX = 0
+                        case 1:
+                            difY = -1
+                            difX = 0
+                        case 2:
+                            difY = 0
+                            difX = -1
+                        case 3:
+                            difY = 0
+                            difX = 1
+                case '_':
+                    if stack.pop() == 0:
+                        difY = 1
+                        difX = 0
+                    else:
+                        difY = -1
+                        difX = 0
+                case '|':
+                    if stack.pop() == 0:
+                        difX = 1
+                        difY = 0
+                    else:
+                        difX = -1
+                        difY = 0
+                case '\"':
+                    stringMode = True
+                case ':':
+                    if len(stack )== 0:
                         stack.append(0)
-                    case 0:
-                        stack.append(0)
-                        stack.append(0)
-                    case _:
+                    else:
                         a = stack.pop()
-                        b = stack.pop()
                         stack.append(a)
-                        stack.append(b)
-            case '$':
-                stack.pop()
-            case '.':
-                output += stack.pop()
-            case ',':
-                output += ord(stack.pop())
-            case '#':
-                x += difX
-                y += difY
-            case 'p':
-                a = stack.pop()
-                b = stack.pop()
-                code[b][a] = stack.pop()
-            case 'g':
-                a = stack.pop()
-                b = stack.pop()
-                stack.append(code[b][a])
-            case _:
-                stack.append(code[x][y])
+                        stack.append(a)
+                case '\\':
+                    match len(stack):
+                        case 1:
+                            stack.append(0)
+                        case 0:
+                            stack.append(0)
+                            stack.append(0)
+                        case _:
+                            a = stack.pop()
+                            b = stack.pop()
+                            stack.append(a)
+                            stack.append(b)
+                case '$':
+                    stack.pop()
+                case '.':
+                    output += stack.pop()
+                case ',':
+                    output += ord(stack.pop())
+                case '#':
+                    x += difX
+                    y += difY
+                case 'p':
+                    a = stack.pop()
+                    b = stack.pop()
+                    code[b][a] = stack.pop()
+                case 'g':
+                    a = stack.pop()
+                    b = stack.pop()
+                    stack.append(code[b][a])
+                case _:
+                    stack.append(code[x][y])
         
         x += difX
         y += difY
 
     # TODO: Interpret the code!
     return output
-
-print(interpret(">987v>.v\nv456<  :\n>321 ^ _@"))
