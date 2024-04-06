2c5f5b7164237b3bf4b2fce950417320f99d503c
AFootGolf9 <adriandosanjos000@gmail.com>
2024-04-05 21:23:37 -0300

    finished the half done addTwoNumbers

--- JavaCode/addTwoNumbers/Soulution.java
+++ JavaCode/addTwoNumbers/Soulution.java
@@ -11,31 +11,28 @@ package addTwoNumbers;
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode temp = l1;
-        int sum = 0;
-        int power = 1;
+        ListNode out = l1;
 
-        while(temp != null){
-            sum += temp.val * power;
-            power = power * 10;
-            temp = temp.next;
+        while(l1 != null && l2 != null){
+            l1.val += l2.val;
+            if(l1.val >= 10){
+                l1.val -= 10;
+                if(l1.next == null){
+                    l1.next = new ListNode(1);
+                }else{
+                    l1.next.val++;
+                }
+            }
+            if(l1.next == null && l2.next != null){
+                l1.next = new ListNode(0);
+            }
+            if(l2.next == null && l1.next != null){
+                l2.next = new ListNode(0);
+            }
+            l1 = l1.next;
+            l2 = l2.next;
         }
 
-        temp = l2;
-        power = 1;
-
-        while(temp != null){
-            sum += temp.val * power;
-            power = power * 10;
-            temp = temp.next;
-        }
-
-        String numString = Integer.toString(sum);
-
-        for(int i = 0; i < numString.length(); i++){
-            temp = new ListNode(numString.charAt(i) - '0', temp);
-        }
-
-        return temp;
+        return out;
     }
 }
